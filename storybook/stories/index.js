0ea490449effa4f0bd6e3b97ab45967ba7f5885d
Dave Hudson <dave@applificationhq.com>
2018-01-09 16:29:00 +0000

    Added conent view story

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -6,9 +6,17 @@ import { linkTo } from '@storybook/addon-links';
 
 import CenterView from './CenterView';
 
-import { Text } from 'react-native-elements-minimalist';
+import { Text, Content } from 'react-native-elements-minimalist';
 
-storiesOf('Minimalist UI', module)
+storiesOf('Minimalist UI Text', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
   .add('Text', () => <Text>Hey Hey Hey!!!</Text>)
   .add('Text Other', () => <Text>Other text</Text>);
+
+storiesOf('Minimalist UI Content', module)
+  .add('DefaultProps (white)', () => <Content />)
+  .add('Passed in Props (pink)', () => (
+    <Content backgroundColor="pink">
+      <Text>Children props</Text>
+    </Content>
+  ));
