e1c41262e9180430998e31d7a8d704c2160e4efa
Dave Hudson <dave@applificationhq.com>
2018-01-09 11:52:52 +0000

    Added ReadMe with instructions on using npm link to set up symbolic link between the projects

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 ## Test app for React-Native-Minimalist
 
+### NPM Link
+
+In order to make this work you need to link react-native-minimalist with react-native-minimalist-app
+
+To do so in root of react-native-minimalist app run:
+
+npm link
+
+In root of react-native-minimalist app run:
+
+npm link react-native-minimalist
+
+This will create a symbolic link and you'll notice there is a react-native-minimalist folder within node_modules just as if you'd installed from NPM.
+
+Now you can make changes in react-native-minimalist, Haul will automatically detect the change, reload and you can refresh your simulator to see latest changes.
+
 ### Running App
 
 This app uses Haul instead of React Native Package Manager because we need Symlinks to work in order to get a decent development flow.
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import Button from './Button';
 import CenterView from './CenterView';
-import Welcome from './Welcome';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import { Text } from 'react-native-elements-minimalist';
 
-storiesOf('Button', module)
+storiesOf('Minimalist UI', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add('with text', () => (
-    <Button onPress={action('clicked-text')}>
-      <Text>Hello Button</Text>
-    </Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onPress={action('clicked-emoji')}>
-      <Text>😀 😎 👍 💯</Text>
-    </Button>
-  ));
+  .add('Text', () => <Text>Hey Hey Hey!!!</Text>)
+  .add('Text Other', () => <Text>Other text</Text>);
