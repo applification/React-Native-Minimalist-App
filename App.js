bf5308d3a442feb9f27ce2f9252b70d318a47d5f
Dave Hudson <dave@applificationhq.com>
2018-01-11 12:22:28 +0000

    Navigation styling

--- App.js
+++ App.js
@@ -3,6 +3,28 @@ import { Navigation } from 'react-native-navigation';
 import { registerScreens } from './screens';
 
 registerScreens();
+const appStyle = {
+  orientation: 'portrait',
+  hideBackButtonTitle: false,
+  navBarLeftButtonColor: '#000000',
+  navBarLeftButtonFontSize: 14,
+  navBarLeftButtonFontWeight: '600',
+  navBarRightButtonColor: '#000000',
+  navBarRightButtonFontSize: 14,
+  navBarRightButtonFontWeight: '600'
+};
+
+const navigatorStyle = {
+  navBarBackgroundColor: '#FAFAFA',
+  navBarButtonColor: '#000000',
+  navBarSelectedButtonColor: '#000000',
+  navBarTextColor: '#000000'
+};
+
+const tabsStyle = {
+  tabBarBackgroundColor: '#FAFAFA',
+  tabBarSelectedButtonColor: '#000000'
+};
 
 Navigation.startTabBasedApp({
   tabs: [
@@ -11,21 +33,39 @@ Navigation.startTabBasedApp({
       screen: 'app.Home',
       icon: require('./imgs/one.png'),
       selectedIcon: require('./imgs/one_selected.png'),
-      title: 'Home'
+      title: 'Instagrammy',
+      iconInsets: {
+        top: 6,
+        left: 0,
+        bottom: -6,
+        right: 0
+      }
     },
     {
-      label: 'Profile',
       screen: 'app.Profile',
       icon: require('./imgs/two.png'),
       selectedIcon: require('./imgs/two_selected.png'),
-      title: 'Profile'
+      title: 'Profile',
+      iconInsets: {
+        top: 6,
+        left: 0,
+        bottom: -6,
+        right: 0
+      }
     },
     {
-      label: 'Settings',
       screen: 'app.Settings',
       icon: require('./imgs/three.png'),
       selectedIcon: require('./imgs/three_selected.png'),
-      title: 'Settings'
+      title: 'Settings',
+      iconInsets: {
+        top: 6,
+        left: 0,
+        bottom: -6,
+        right: 0
+      }
     }
-  ]
+  ],
+  appStyle,
+  tabsStyle
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AlertIOS } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import { Content, HeaderText, BodyText } from 'react-native-minimalist';
 
+const MenuIcon = require('../imgs/navicon_menu.png');
+
 class HomeScreen extends Component {
+  static navigatorStyle = {
+    navBarBackgroundColor: '#FAFAFA',
+    navBarButtonColor: '#000000',
+    navBarSelectedButtonColor: '#000000',
+    navBarTextColor: '#000000',
+    navBarTitleFontFamily: 'SavoyeLetPlain',
+    navBarTitleFontSize: 34
+  };
+
+  static navigatorButtons = {
+    leftButtons: [
+      {
+        id: 'menu',
+        title: 'Menu',
+        icon: MenuIcon
+      }
+    ],
+    rightButtons: [
+      {
+        id: 'edit',
+        testID: 'edit',
+        systemItem: 'search'
+      }
+    ]
+  };
+
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+  }
+
+  onNavigatorEvent(event) {
+    if (event.type === 'NavBarButtonPress') {
+      if (event.id === 'edit') {
+        AlertIOS.alert('NavBar', 'Edit button pressed!!');
+      } else if (event.id === 'search') {
+        AlertIOS.alert('NavBar', 'Search button pressed!!');
+      }
+    }
+  }
+
   render() {
     return (
       <Content>
